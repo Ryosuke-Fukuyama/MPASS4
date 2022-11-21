4bbeba3b61bf4461935d53bd79056d2009c26901
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-11-21 11:29:30 +0900

    [fix] #59 issue14

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports. # エラー画面テストの際には false に変更！
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable server timing
   config.server_timing = true
