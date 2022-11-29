5e4b6cf18e09a114efc6a25cf5cc663b04c5fcab
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-11-29 20:46:51 +0900

    [fix] #69 vue css適用

--- app/javascript/ReverseListSet.vue
+++ app/javascript/ReverseListSet.vue
@@ -118,9 +118,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.noshow__list {
-  color: red;
-}
-</style>
--- app/javascript/components/NumList.vue
+++ app/javascript/components/NumList.vue
@@ -3,26 +3,25 @@
     <li v-for="health_interview in classificationHealthInterviews" :key="health_interview.id">
       <div v-if="!sessionCheck">{{ health_interview.guide_status.id }}</div>
 
-      <div v-if="sessionCheck">
-        <span>
-          <div @click="linkToHealthInterview(health_interview)" class="link__to font__large">
-            {{ health_interview.guide_status.id }}
-          </div><select
-            v-model.lazy="health_interview.guide_status.status"
-            @change="selectStatus(health_interview, $event)"
-          >
-            <option value="initial">初期</option>
-            <option value="calling">呼出</option>
-            <option value="pending">保留</option>
-            <option value="noshow">無断キャンセル</option>
-            <option value="payment">決済</option>
-          </select><div
-            v-if="modalFlag && (missId === health_interview.id)"
-            class="modal__button"
-          >
-            <ErrorModal @close-click="informClick()"></ErrorModal>
-          </div>
-        </span>
+      <div v-if="sessionCheck" class="div__flex">
+        <div @click="linkToHealthInterview(health_interview)" class="link__to font__large">
+          {{ health_interview.guide_status.id }}
+        </div><select
+          v-model.lazy="health_interview.guide_status.status"
+          @change="selectStatus(health_interview, $event)"
+          class="vue__select"
+        >
+          <option value="initial">初期</option>
+          <option value="calling">呼出</option>
+          <option value="pending">保留</option>
+          <option value="noshow">無断キャンセル</option>
+          <option value="payment">決済</option>
+        </select><div
+          v-if="modalFlag && (missId === health_interview.id)"
+          class="modal__button"
+        >
+          <ErrorModal @close-click="informClick()"></ErrorModal>
+        </div>
       </div>
     </li>
   </div>
@@ -97,32 +96,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  span {
-    display: -webkit-flex;
-    display: flex;
-  }
-
-  select {
-    width: 55px;
-    border-radius: 10px;
-  }
-
-  .font__large {
-    font-size: large;
-  }
-
-  .link__to {
-    margin-right: 2px;
-    text-decoration: underline;
-  }
-
-  .link__to:hover {
-    color: blue;
-  }
-
-  /* .modal__button {
-    display: inline-flex;
-  } */
-</style>
