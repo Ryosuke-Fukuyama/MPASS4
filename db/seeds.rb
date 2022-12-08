8e4d421312c9a971bfc48b33d5bf324de7468d24
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-12-08 18:44:28 +0900

    [fix] #75 deploy時db:resetしてからseed生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,42 +58,42 @@ end
   )
 end
 
-# Patient.eager_load(:health_interviews).all.each do |p|
-#   symptoms =  '(サンプル)３日前から発熱、倦怠感と吐き気'
-#   condition = '(サンプル)持病等'
+Patient.eager_load(:health_interviews).all.each do |p|
+  symptoms =  '(サンプル)３日前から発熱、倦怠感と吐き気'
+  condition = '(サンプル)持病等'
 
-#   HealthInterview.create!(
-#     age:         "#{1 + rand(100)}",
-#     gender:      0,
-#     condition:   condition,
-#     symptoms:    symptoms,
-#     patient_id:  p.id,
-#     hospital_id: 2
-#   )
-# end
-# Patient.eager_load(:health_interviews).all.each do |p|
-#   symptoms =  '(サンプル)３日前から発熱、倦怠感と吐き気'
-#   condition = '(サンプル)持病等'
+  HealthInterview.create!(
+    age:         "#{1 + rand(100)}",
+    gender:      0,
+    condition:   condition,
+    symptoms:    symptoms,
+    patient_id:  p.id,
+    hospital_id: 2
+  )
+end
+Patient.eager_load(:health_interviews).all.each do |p|
+  symptoms =  '(サンプル)３日前から発熱、倦怠感と吐き気'
+  condition = '(サンプル)持病等'
 
-#   HealthInterview.create!(
-#     age:         "#{1 + rand(100)}",
-#     gender:      0,
-#     condition:   condition,
-#     symptoms:    symptoms,
-#     patient_id:  p.id,
-#     hospital_id: 1
-#   )
-# end
-# # 周回すると一つのHealthInterviewに複数のGuideStatusが付いてバグる。
-# HealthInterview.eager_load(:guide_status).where(health_interviews: { hospital_id: 2 }).all.each do |h|
-#   GuideStatus.create!(
-#     status:             'complete',
-#     health_interview_id: h.id
-#   )
-# end
-# HealthInterview.eager_load(:guide_status).where(health_interviews: { hospital_id: 1 }).all.each do |h|
-#   GuideStatus.create!(health_interview_id: h.id)
-# end
+  HealthInterview.create!(
+    age:         "#{1 + rand(100)}",
+    gender:      0,
+    condition:   condition,
+    symptoms:    symptoms,
+    patient_id:  p.id,
+    hospital_id: 1
+  )
+end
+# 周回すると一つのHealthInterviewに複数のGuideStatusが付いてバグる。
+HealthInterview.eager_load(:guide_status).where(health_interviews: { hospital_id: 2 }).all.each do |h|
+  GuideStatus.create!(
+    status:             'complete',
+    health_interview_id: h.id
+  )
+end
+HealthInterview.eager_load(:guide_status).where(health_interviews: { hospital_id: 1 }).all.each do |h|
+  GuideStatus.create!(health_interview_id: h.id)
+end
 
 # # ゲストログイン実装後不要
 # Master.create!(
