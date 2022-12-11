0a360065a9ddf6447c9822003448e82ef8b99545
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-12-11 09:20:50 +0900

    [fix] #77 depioy時seed ユニークエラー？

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :rbenv_ruby, '3.0.5'
 # set :rbenv_custom_path, ''
 set :log_level, :info
 
-after 'deploy:published', 'deploy:seed'
+# after 'deploy:published', 'deploy:seed'
 after 'deploy:finished', 'deploy:restart'
 
 namespace :deploy do
@@ -53,7 +53,7 @@ namespace :deploy do
     on roles(:db) do
       with rails_env: fetch(:rails_env) do
         within current_path do
-          execute :bundle, :exec, :rake, 'db:migrate:reset'
+          # execute :bundle, :exec, :rake, 'db:migrate:reset'
           execute :bundle, :exec, :rake, 'db:seed'
         end
       end
