e88ad918e0f15f9c3d3a56166332d9290e0e0e29
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-12-20 21:42:47 +0900

    [update] #86 letter_opener設定

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,24 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['GMAIL'],
+  password: ENV['GOOGLE_APP_PASSWORD'],
+  # domain: 'admin.example.com',
+  address: 'smtp.gmail.com',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+
+# ActionMailer::Base.smtp_settings = {
+#   user_name: ENV['SENDGRID_USER_NAME'],
+#   password: ENV['SENDGRID_PASSWORD'],
+#   domain: 'admin.example.com',
+#   address: 'smtp.sendgrid.net',
+#   port: 587,
+#   authentication: :plain,
+#   enable_starttls_auto: true
+# }
